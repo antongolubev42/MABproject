20761ef601980b0cd152f3277b3e7885caacf0e9
Anton Golubev <anton.g421@gmail.com>
2020-12-08 17:38:33 +0000

    working on pickups

---
+++ Assets/Scripts/Sausage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SausagePickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag=="Player")
+        {
+            Destroy(gameObject);
+            Debug.Log("sasig");
+
+        }
+    }
+   
+}
---
+++ Assets/Scripts/SausagePickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sausage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int r= Random.Range(1,3);
+        if(r==1)
+        {
+            transform.position=new Vector3(-7,transform.position.y,transform.position.z);
+        }
+        else if (r==2)
+        {
+            transform.position=new Vector3(0,transform.position.y,transform.position.z);
+        }
+
+        else 
+        {
+            transform.position=new Vector3(+7,transform.position.y,transform.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag=="Player")
+        {
+            Destroy(gameObject);
+            Debug.Log("sasig");
+
+        }
+    }
+   
+}
